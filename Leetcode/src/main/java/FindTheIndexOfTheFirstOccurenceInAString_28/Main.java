fd7e778551fac223ba36bebb67f488f83580b894
Baris Algun <bbarisalgun@gmail.com>
2024-02-21 22:38:15 +0300

    findTheIndexOfTheFırstOccurenceInAString_28

---
+++ Leetcode/src/main/java/FindTheIndexOfTheFirstOccurenceInAString_28/Main.java
@@ -0,0 +1,18 @@
+package FindTheIndexOfTheFirstOccurenceInAString_28;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+
+        String haystack = "sadbutsad", needle = "sad";
+
+
+        System.out.println(solution.strStr(haystack,needle));
+
+
+        }
+
+
+}
