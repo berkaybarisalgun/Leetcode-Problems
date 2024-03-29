d7a0c1c930ea750f32ed2c367a2c1b5bc4b8a169
Berkay Baris Algun <bbarisalgun@gmail.com>
2024-03-29 10:27:15 +0300

    Solution with CHATGPT

--- Leetcode/src/main/java/LongestConsecutiveSequence_128/Main.java
+++ Leetcode/src/main/java/LongestConsecutiveSequence_128/Main.java
@@ -2,10 +2,10 @@ package LongestConsecutiveSequence_128;
 
 public class Main {
     public static void main(String[] args) {
-        Solution solution=new Solution();
+        Solution2 solution2=new Solution2();
 
-        int[] intArray = {100,4,200,1,3,2};
+        int[] intArray = {100,4,101,2,3,1};
 
-        System.out.println(solution.longestConsecutive(intArray));
+        System.out.println(solution2.longestConsecutive(intArray));
     }
 }
