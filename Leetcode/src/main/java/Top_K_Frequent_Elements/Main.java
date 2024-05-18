8653949b55688ac5cdb9ea3997db999eb8d6f137
Baris Algun <76767449+berkaybarisalgun@users.noreply.github.com>
2024-05-19 01:04:17 +0300

    Top_K_Frequent_Elements_347
    Top_K_Frequent_Elements_347

---
+++ Leetcode/src/main/java/Top_K_Frequent_Elements/Main.java
@@ -0,0 +1,12 @@
+package Top_K_Frequent_Elements;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Solution solution=new Solution();
+        int[] nums = {1, 1, 1, 2, 2, 3};
+
+        solution.topKFrequent(nums,2);
+
+    }
+}
---
+++ Leetcode/src/main/java/Top_K_Frequent_Elements/Solution.java
@@ -0,0 +1,31 @@
+package Top_K_Frequent_Elements;
+
+import java.util.*;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        int[] arr = new int[k];
+        HashMap<Integer, Integer> hash = new HashMap<>();
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((n1, n2) -> hash.get(n1) - hash.get(n2));// hangisi daha küçükse ona öncelik veriyor. default olarak.
+
+        for (Integer num : nums) {
+            hash.put(num, hash.getOrDefault(num, 0) + 1);
+        }
+
+        for (int n : hash.keySet()) {
+            priorityQueue.add(n);
+        }
+        System.out.println(priorityQueue+"\n");
+        System.out.println(priorityQueue.element());
+
+
+        for (int i = 0; i < k; i++) {
+            arr[i] = priorityQueue.poll();
+        }
+
+
+        return arr;
+
+    }
+}
